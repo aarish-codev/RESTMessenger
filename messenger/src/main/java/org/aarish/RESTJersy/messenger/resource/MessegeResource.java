c4833307e687f71f1ee7cf7b856f797ae1e78a53
aarishS <aarish.work@gmail.com>
2019-05-11 22:40:09 +0530

    Revert "added POST WS"
    This reverts commit 84a9bc068e3313f44ff65818147848f5a068f27a.

--- messenger/src/main/java/org/aarish/RESTJersy/messenger/resource/MessegeResource.java
+++ messenger/src/main/java/org/aarish/RESTJersy/messenger/resource/MessegeResource.java
@@ -2,7 +2,6 @@ package org.aarish.RESTJersy.messenger.resource;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -31,14 +30,6 @@ public class MessegeResource {
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return service.getMessage(messageId);
 	}
-	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message addMessage(Message message){
-		return service.addMessage(message);
-	}
-	
 	
 
 }
