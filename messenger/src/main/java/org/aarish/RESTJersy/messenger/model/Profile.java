4e2111cb4574970055c329d63c483279904a3e60
aarishS <aarish.work@gmail.com>
2019-05-13 22:36:39 +0530

    added Profile WS

--- messenger/src/main/java/org/aarish/RESTJersy/messenger/model/Profile.java
+++ messenger/src/main/java/org/aarish/RESTJersy/messenger/model/Profile.java
@@ -20,14 +20,55 @@ public class Profile {
 		
 	}
 
-	public Profile(Long id, String profileName, String firstName, String lastName, Date created) {
+	public Profile(Long id, String profileName, String firstName, String lastName) {
 		super();
 		this.id = id;
 		this.profileName = profileName;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.created = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
 		this.created = created;
 	}
+	
 	
 	
 	
